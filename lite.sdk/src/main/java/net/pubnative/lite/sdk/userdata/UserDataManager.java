1a509b4202324886de84ed95d69893470725e86a
eros902002@googlemail.com <eros902002@googlemail.com>
2018-05-16 16:42:47 +0200

    Add privacy policy and vendor list links

--- lite.sdk/src/main/java/net/pubnative/lite/sdk/userdata/UserDataManager.java
+++ lite.sdk/src/main/java/net/pubnative/lite/sdk/userdata/UserDataManager.java
@@ -1,5 +1,23 @@
 package net.pubnative.lite.sdk.userdata;
 
 public class UserDataManager {
+    private static final String LANGUAGE_ENGLISH = "en";
 
+    public String getPrivacyPolicyLink() {
+        return getPrivacyPolicyLink(LANGUAGE_ENGLISH);
+    }
+
+    public String getPrivacyPolicyLink(String language) {
+        String baseUrl = "https://pubnative.net/privacy-policy/";
+        return baseUrl;
+    }
+
+    public String getVendorListLink() {
+        return getVendorListLink(LANGUAGE_ENGLISH);
+    }
+
+    public String getVendorListLink(String language) {
+        String baseUrl = "https://pubnative.net/vendor-list/";
+        return baseUrl;
+    }
 }
