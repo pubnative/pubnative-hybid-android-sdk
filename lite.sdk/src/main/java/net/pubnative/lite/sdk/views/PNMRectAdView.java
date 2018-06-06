59db61b89443c78c5ce78412e43e081d33ff1644
eros902002@googlemail.com <eros902002@googlemail.com>
2018-06-06 16:53:01 +0200

    Create PNAdView

---
+++ lite.sdk/src/main/java/net/pubnative/lite/sdk/views/PNMRectAdView.java
@@ -0,0 +1,80 @@
+package net.pubnative.lite.sdk.views;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import net.pubnative.lite.sdk.api.MRectRequestManager;
+import net.pubnative.lite.sdk.api.RequestManager;
+import net.pubnative.lite.sdk.models.Ad;
+import net.pubnative.lite.sdk.mrect.presenter.MRectPresenter;
+import net.pubnative.lite.sdk.mrect.presenter.MRectPresenterFactory;
+import net.pubnative.lite.sdk.utils.Logger;
+
+public class PNMRectAdView extends PNAdView implements MRectPresenter.Listener {
+
+    private MRectPresenter mPresenter;
+
+    public PNMRectAdView(Context context) {
+        super(context);
+    }
+
+    public PNMRectAdView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PNMRectAdView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public PNMRectAdView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void cleanup() {
+        super.cleanup();
+        if (mPresenter != null) {
+            mPresenter.destroy();
+            mPresenter = null;
+        }
+    }
+
+    @Override
+    protected String getLogTag() {
+        return PNMRectAdView.class.getSimpleName();
+    }
+
+    @Override
+    protected RequestManager getRequestManager() {
+        return new MRectRequestManager();
+    }
+
+    @Override
+    protected void renderAd() {
+        mPresenter = new MRectPresenterFactory(getContext())
+                .createMRectPresenter(mAd, this);
+    }
+
+    //------------------------------ MRectPresenter Callbacks --------------------------------------
+    @Override
+    public void onMRectLoaded(MRectPresenter mRectPresenter, View mRect) {
+        if (mRect == null) {
+            invokeOnLoadFailed(new Exception("An error has occurred while rendering the ad"));
+        } else {
+            setupAdView(mRect);
+        }
+    }
+
+    @Override
+    public void onMRectError(MRectPresenter mRectPresenter) {
+        invokeOnLoadFailed(new Exception("An error has occurred while rendering the ad"));
+    }
+
+    @Override
+    public void onMRectClicked(MRectPresenter mRectPresenter) {
+        invokeOnClick();
+    }
+}
