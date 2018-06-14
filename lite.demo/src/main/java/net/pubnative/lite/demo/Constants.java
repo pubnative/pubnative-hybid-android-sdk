3b43e2022d7ba0bc2c8c4984f6dbb13e1d4894ab
eros902002@googlemail.com <eros902002@googlemail.com>
2018-06-14 12:53:46 +0200

    Reorganise UI packages and add mediation ad units

--- lite.demo/src/main/java/net/pubnative/lite/demo/Constants.java
+++ lite.demo/src/main/java/net/pubnative/lite/demo/Constants.java
@@ -14,6 +14,10 @@ public class Constants {
     public static final String INTERSTITIAL_VIDEO_ZONE_ID = "4";
     public static final String MEDIUM_VIDEO_ZONE_ID = "6";
 
+    public static final String MOPUB_MEDIATION_BANNER_AD_UNIT = "38864d35877d4684af11319530993074";
+    public static final String MOPUB_MEDIATION_INTERSTITIAL_AD_UNIT = "2bb6db4d69fa407a87b0c96c55d7c2b4";
+    public static final String MOPUB_MEDIATION_MEDIUM_AD_UNIT = "0af490cac3d34256b0010fac17f26759";
+
     public static final String MOPUB_MRAID_BANNER_AD_UNIT = "b8b82260e1b84a9ba361e03c21ce4caf";
     public static final String MOPUB_MRAID_INTERSTITIAL_AD_UNIT = "0bd7ea20185547f2bd29a9574bfce917";
     public static final String MOPUB_MRAID_MEDIUM_AD_UNIT = "1fafef6b872a4e10ba9fc573ca347e55";
---
+++ lite.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/config/DFPSettingsFragment.kt
@@ -0,0 +1,58 @@
+package net.pubnative.lite.demo.ui.fragments.config
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import net.pubnative.lite.demo.R
+import net.pubnative.lite.demo.managers.SettingsManager
+
+/**
+ * Created by erosgarciaponte on 30.01.18.
+ */
+class DFPSettingsFragment : Fragment() {
+
+    private lateinit var bannerInput: EditText
+    private lateinit var mediumInput: EditText
+    private lateinit var interstitialInput: EditText
+    private lateinit var settingManager: SettingsManager
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.fragment_dfp_settings, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bannerInput = view.findViewById(R.id.input_dfp_banner)
+        mediumInput = view.findViewById(R.id.input_dfp_medium)
+        interstitialInput = view.findViewById(R.id.input_dfp_interstitial)
+
+        settingManager = SettingsManager.getInstance(context!!)
+
+        view.findViewById<Button>(R.id.button_save_dfp_settings).setOnClickListener {
+            val bannerAdUnitId = bannerInput.text.toString()
+            val mediumAdUnitId = mediumInput.text.toString()
+            val interstitialAdUnitId = interstitialInput.text.toString()
+
+            settingManager.setDFPBannerAdUnitId(bannerAdUnitId)
+            settingManager.setDFPMediumAdUnitId(mediumAdUnitId)
+            settingManager.setDFPInterstitialAdUnitId(interstitialAdUnitId)
+
+            Toast.makeText(activity, "DFP settings saved successfully.", Toast.LENGTH_SHORT).show()
+            activity?.finish()
+        }
+
+        fillSavedValues()
+    }
+
+    private fun fillSavedValues() {
+        val settings = settingManager.getSettings()
+        bannerInput.setText(settings.dfpBannerAdUnitId)
+        mediumInput.setText(settings.dfpMediumAdUnitId)
+        interstitialInput.setText(settings.dfpInterstitialAdUnitId)
+    }
+}
