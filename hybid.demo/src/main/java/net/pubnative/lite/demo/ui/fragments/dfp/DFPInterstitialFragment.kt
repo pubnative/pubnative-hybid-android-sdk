f26f884887eb8a06903290abbb791f4079122be9
eros902002@googlemail.com <eros902002@googlemail.com>
2018-08-08 12:48:48 +0200

    Add impression id labels to DFP pre-bid screens

--- hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/dfp/DFPInterstitialFragment.kt
+++ hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/dfp/DFPInterstitialFragment.kt
@@ -24,11 +24,13 @@ package net.pubnative.lite.demo.ui.fragments.dfp
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest
@@ -53,14 +55,15 @@ class DFPInterstitialFragment : Fragment(), RequestManager.RequestListener {
     private var adUnitId: String? = null
 
     private lateinit var loadButton: Button
+    private lateinit var impressionIdView: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-            = inflater.inflate(R.layout.fragment_dfp_interstitial, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_dfp_interstitial, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         loadButton = view.findViewById(R.id.button_load)
+        impressionIdView = view.findViewById(R.id.view_impression_id)
 
         adUnitId = SettingsManager.getInstance(activity!!).getSettings().dfpInterstitialAdUnitId
 
@@ -99,6 +102,10 @@ class DFPInterstitialFragment : Fragment(), RequestManager.RequestListener {
 
         val adRequest = builder.build()
         dfpInterstitial.loadAd(adRequest)
+
+        if (!TextUtils.isEmpty(ad?.impressionId)) {
+            impressionIdView.text = ad?.impressionId
+        }
         Log.d(TAG, "onRequestSuccess")
     }
 
--- hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/dfp/DFPMRectFragment.kt
+++ hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/dfp/DFPMRectFragment.kt
@@ -24,12 +24,14 @@ package net.pubnative.lite.demo.ui.fragments.dfp
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdSize
@@ -56,14 +58,15 @@ class DFPMRectFragment : Fragment(), RequestManager.RequestListener {
     private lateinit var dfpMRect: PublisherAdView
     private lateinit var dfpMRectContainer: FrameLayout
     private lateinit var loadButton: Button
+    private lateinit var impressionIdView: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
-            = inflater.inflate(R.layout.fragment_dfp_mrect, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_dfp_mrect, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         loadButton = view.findViewById(R.id.button_load)
+        impressionIdView = view.findViewById(R.id.view_impression_id)
         dfpMRectContainer = view.findViewById(R.id.dfp_mrect_container)
 
         requestManager = MRectRequestManager()
@@ -109,6 +112,10 @@ class DFPMRectFragment : Fragment(), RequestManager.RequestListener {
 
         val adRequest = builder.build()
         dfpMRect.loadAd(adRequest)
+
+        if (!TextUtils.isEmpty(ad?.impressionId)) {
+            impressionIdView.text = ad?.impressionId
+        }
         Log.d(TAG, "onRequestSuccess")
     }
 
