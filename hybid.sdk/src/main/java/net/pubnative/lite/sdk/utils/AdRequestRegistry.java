705755cba69f855be6a7effe531c3805ecc35d90
eros902002@googlemail.com <eros902002@googlemail.com>
2018-08-15 18:16:29 +0200

    Create AdRequestRegistry

---
+++ hybid.sdk/src/main/java/net/pubnative/lite/sdk/utils/AdRequestRegistry.java
@@ -0,0 +1,64 @@
+package net.pubnative.lite.sdk.utils;
+
+public class AdRequestRegistry {
+    private RequestItem mLastAdRequest;
+
+    private static AdRequestRegistry sInstance;
+
+    private AdRequestRegistry() {
+
+    }
+
+    public static AdRequestRegistry getInstance() {
+        if (sInstance == null) {
+            synchronized (AdRequestRegistry.class) {
+                if (sInstance == null) {
+                    sInstance = new AdRequestRegistry();
+                }
+            }
+        }
+        return sInstance;
+    }
+
+    public void setLastAdRequest(RequestItem item) {
+        this.mLastAdRequest = item;
+    }
+
+    public RequestItem getLastAdRequest() {
+        return mLastAdRequest;
+    }
+
+    public class RequestItem {
+        private String mUrl;
+        private String mPostParams;
+        private String mResponse;
+        private long mLatency;
+
+        public RequestItem(String url, String response, long latency) {
+            this(url, null, response, latency);
+        }
+
+        public RequestItem(String url, String postParams, String response, long latency) {
+            this.mUrl = url;
+            this.mPostParams = postParams;
+            this.mResponse = response;
+            this.mLatency = latency;
+        }
+
+        public String getUrl() {
+            return mUrl;
+        }
+
+        public String getPostParams() {
+            return mPostParams;
+        }
+
+        public String getResponse() {
+            return mResponse;
+        }
+
+        public long getLatency() {
+            return mLatency;
+        }
+    }
+}
