47940f9652e5ce5a366651ef5360f2c971ad7034
eros902002@googlemail.com <eros902002@googlemail.com>
2018-08-20 15:37:40 +0200

    Set up settings fragment

--- hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/navigation/SettingsNavFragment.kt
+++ hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/navigation/SettingsNavFragment.kt
@@ -1,16 +1,37 @@
 package net.pubnative.lite.demo.ui.fragments.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import net.pubnative.lite.demo.R
+import net.pubnative.lite.demo.ui.activities.config.DFPSettingsActivity
+import net.pubnative.lite.demo.ui.activities.config.HyBidSettingsActivity
+import net.pubnative.lite.demo.ui.activities.config.MoPubSettingsActivity
 
 class SettingsNavFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_nav_settings, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_pn_settings).setOnClickListener {
+            val intent = Intent(activity, HyBidSettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.button_mopub_settings).setOnClickListener {
+            val intent = Intent(activity, MoPubSettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        view.findViewById<Button>(R.id.button_dfp_settings).setOnClickListener {
+            val intent = Intent(activity, DFPSettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
