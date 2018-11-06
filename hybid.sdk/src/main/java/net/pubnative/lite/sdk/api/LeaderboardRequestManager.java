e74c6d5df571cc3bc1bc62b0a30aa6a46d3299c0
eros902002@googlemail.com <eros902002@googlemail.com>
2018-11-06 12:22:50 +0100

    Create leaderboard ad view

---
+++ hybid.sdk/src/main/java/net/pubnative/lite/sdk/api/LeaderboardRequestManager.java
@@ -0,0 +1,8 @@
+package net.pubnative.lite.sdk.api;
+
+public class LeaderboardRequestManager extends RequestManager {
+    @Override
+    protected String getAdSize() {
+        return "s";
+    }
+}
