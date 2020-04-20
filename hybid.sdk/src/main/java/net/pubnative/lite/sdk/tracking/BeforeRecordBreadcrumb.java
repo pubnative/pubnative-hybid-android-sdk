b9714fae9423bc43218fcc3eb1afaa9f38d2dd10
Pablo <pablo.aldehuela@pubnative.net>
2020-04-20 15:33:50 +0200

    - Deleted whole tracking folder - Deleted HyBidCrashTracker init in HyBid.java

--- hybid.sdk/src/main/java/net/pubnative/lite/sdk/tracking/BeforeRecordBreadcrumb.java
+++
@@ -1,9 +0,0 @@
-package net.pubnative.lite.sdk.tracking;
-
-/**
- * Created by erosgarciaponte on 12.02.18.
- */
-
-public interface BeforeRecordBreadcrumb {
-    boolean shouldRecord(Breadcrumb breadcrumb);
-}
