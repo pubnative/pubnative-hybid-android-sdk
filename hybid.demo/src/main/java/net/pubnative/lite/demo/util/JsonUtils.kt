aa2477ef918feceb96a1f4f6a5e7cff1351be4f6
eros902002@googlemail.com <eros902002@googlemail.com>
2020-07-03 11:09:27 +0200

    Check for valid JSON on demo app debug fragment

--- hybid.demo/src/main/java/net/pubnative/lite/demo/util/JsonUtils.kt
+++ hybid.demo/src/main/java/net/pubnative/lite/demo/util/JsonUtils.kt
@@ -1,17 +1,25 @@
 package net.pubnative.lite.demo.util
 
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonParseException
 import com.google.gson.JsonParser
+import net.pubnative.lite.sdk.utils.Logger
 
 class JsonUtils {
     companion object {
+        val TAG = JsonUtils::class.java.simpleName
+
         @JvmStatic
         fun toFormattedJson(json: String): String {
-            val parser = JsonParser()
-            val jsonObject = parser.parse(json).asJsonObject
+            try {
+                val jsonObject = JsonParser.parseString(json).asJsonObject
 
-            val gson = GsonBuilder().setLenient().setPrettyPrinting().create()
-            return gson.toJson(jsonObject)
+                val gson = GsonBuilder().setLenient().setPrettyPrinting().create()
+                return gson.toJson(jsonObject)
+            } catch (exception: JsonParseException) {
+                Logger.e(TAG, "Error parsing json: ", exception)
+                return json
+            }
         }
     }
 }
