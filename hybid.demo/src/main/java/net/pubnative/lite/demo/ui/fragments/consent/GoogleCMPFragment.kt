ab43ba74dbcead86e04dd2dab6a36e84e96afcc9
eros902002@googlemail.com <eros902002@googlemail.com>
2020-09-14 12:16:39 +0200

    Add google cmp to gradle

--- hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/consent/GoogleCMPFragment.kt
+++ hybid.demo/src/main/java/net/pubnative/lite/demo/ui/fragments/consent/GoogleCMPFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import net.pubnative.lite.demo.R
 
 class GoogleCMPFragment : Fragment() {
+    private val TAG = GoogleCMPFragment::class.java.simpleName
+    
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_google_cmp, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
