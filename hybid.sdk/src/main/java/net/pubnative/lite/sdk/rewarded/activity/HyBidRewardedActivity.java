0a4c0106e06b2404d6b0421fa7919a8402a5aa7a
eros902002@googlemail.com <eros902002@googlemail.com>
2020-10-15 18:23:01 +0200

    Setup RewardedVideo activities

--- hybid.sdk/src/main/java/net/pubnative/lite/sdk/rewarded/activity/HyBidRewardedActivity.java
+++ hybid.sdk/src/main/java/net/pubnative/lite/sdk/rewarded/activity/HyBidRewardedActivity.java
@@ -1,6 +1,156 @@
 package net.pubnative.lite.sdk.rewarded.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+
+import net.pubnative.lite.sdk.HyBid;
+import net.pubnative.lite.sdk.models.Ad;
+import net.pubnative.lite.sdk.rewarded.HyBidRewardedBroadcastReceiver;
+import net.pubnative.lite.sdk.rewarded.HyBidRewardedBroadcastSender;
+import net.pubnative.lite.sdk.utils.UrlHandler;
+import net.pubnative.lite.sdk.views.CloseableContainer;
 
 public abstract class HyBidRewardedActivity extends Activity {
+    public static final String EXTRA_ZONE_ID = "extra_pn_zone_id";
+    public static final String EXTRA_BROADCAST_ID = "extra_pn_broadcast_id";
+
+    private CloseableContainer mCloseableContainer;
+    private UrlHandler mUrlHandlerDelegate;
+    private Ad mAd;
+    private String mZoneId;
+    private HyBidRewardedBroadcastSender mBroadcastSender;
+    private ProgressBar progressBar;
+
+    public abstract View getAdView();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mUrlHandlerDelegate = new UrlHandler(this);
+        mZoneId = intent.getStringExtra(EXTRA_ZONE_ID);
+        long broadcastId = intent.getLongExtra(EXTRA_BROADCAST_ID, -1);
+
+        if (!TextUtils.isEmpty(mZoneId) && broadcastId != -1) {
+            mBroadcastSender = new HyBidRewardedBroadcastSender(this, broadcastId);
+
+            View adView = getAdView();
+
+            if (adView != null) {
+
+                mCloseableContainer = new CloseableContainer(this);
+                mCloseableContainer.setCloseVisible(false);
+                mCloseableContainer.setOnCloseListener(new CloseableContainer.OnCloseListener() {
+                    @Override
+                    public void onClose() {
+                        dismiss();
+                    }
+                });
+
+                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+                params.gravity = Gravity.CENTER;
+
+                progressBar = new ProgressBar(this);
+                setProgressBarInvisible();
+
+                FrameLayout.LayoutParams pBarParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+                pBarParams.gravity = Gravity.CENTER;
+
+                mCloseableContainer.addView(progressBar, pBarParams);
+
+                mCloseableContainer.addView(adView, params);
+                mCloseableContainer.setBackgroundColor(Color.WHITE);
+
+                if (getAd() != null) {
+                    View contentInfo = getAd().getContentInfoContainer(this);
+                    if (contentInfo != null) {
+                        mCloseableContainer.addView(contentInfo);
+                    }
+                }
+
+                setContentView(mCloseableContainer);
+            } else {
+                finish();
+            }
+        } else {
+            finish();
+        }
+    }
+
+    protected void dismiss() {
+        getBroadcastSender().sendBroadcast(HyBidRewardedBroadcastReceiver.Action.CLOSE);
+        finish();
+    }
+
+    protected String getZoneId() {
+        return mZoneId;
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mCloseableContainer != null) {
+            mCloseableContainer.removeAllViews();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        dismiss();
+    }
+
+    protected CloseableContainer getCloseableContainer() {
+        return mCloseableContainer;
+    }
+
+    protected void showRewardedCloseButton() {
+        if (mCloseableContainer != null) {
+            mCloseableContainer.setCloseVisible(true);
+        }
+    }
+
+    protected void hideRewardedCloseButton() {
+        if (mCloseableContainer != null) {
+            mCloseableContainer.setCloseVisible(false);
+        }
+    }
+
+    protected UrlHandler getUrlHandler() {
+        return mUrlHandlerDelegate;
+    }
+
+    protected Ad getAd() {
+        if (mAd == null) {
+            if (HyBid.getAdCache() != null) {
+                mAd = HyBid.getAdCache().remove(mZoneId);
+            }
+        }
+        return mAd;
+    }
+
+    protected HyBidRewardedBroadcastSender getBroadcastSender() {
+        return mBroadcastSender;
+    }
+
+    protected void setProgressBarVisible() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    protected void setProgressBarInvisible() {
+        progressBar.setVisibility(View.INVISIBLE);
+    }
 }
